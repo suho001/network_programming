9dd981837dc2b7370138de9e133902b91ec695d7
Shin suho <tngh0203@gmail.com>
2023-10-09 15:02:47 +0900

    upload

--- TCPsocket/echo_Client.py
+++ TCPsocket/echo_Client.py
@@ -1,28 +1,29 @@
-#TCP Echo Server Program
-from socket import *
-
-port = 2700		# 포트 번호
-BUFSIZE = 1024	# 수신 버퍼 사이즈
-
-sock = socket(AF_INET, SOCK_STREAM)
-sock.bind(('', port))	# 종단점과 소켓 결합. ' 임의 주소'
-sock.listen(1)
-conn, (remotehost, remoteport) = sock.accept() # 연결소켓, 연결 주소(IP 주소 포트번호) 반환
-print('connected by', remotehost, remoteport)
+import socket
+# port = int(input("Port No: "))
+PORT = 2500
+HOST = 'localhost'
+address = (HOST, PORT) # 주소는 항상 (ip, port) 튜플
+BUFSIZE = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(address) # 서버 연결 요청
 while True:
-    try:
-        data = conn.recv(BUFSIZE) # 데이터 수신
-    except:
-        conn.close()
-        break
-    else:
-        if not data:  # ''이면 종료, ''는 False
-            break
-        print("Received message: ", data.decode())  # 수신 데이터 출력. 바이트형으로 수신됨으로 문자열로 변환#
+	msg = input("Message to send: ")
 
-    try:
-        conn.send(data)# 수신 데이터를 되돌려 전송
-    except:
-        conn.close()
-        break
-conn.close()
+	if not msg:
+		continue
+	try:
+		s.send(msg.encode()) #send a message to server
+	except:
+		print("연결 종료")
+		break
+
+	try: # 데이터 읽기
+		data = s.recv(BUFSIZE) #receive message from server
+		if not data:
+			print("연결 종료")
+			break
+		print("Received message: %s" %data.decode()) # 바이트형을 문자열로 변환
+	except:# 연결 종료
+		print("연결 종료")
+		break
+s.close()

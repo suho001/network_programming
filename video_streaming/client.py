a5078888061da2fe960a782ea04ee5bcb5e942c8
Shin suho <tngh0203@gmail.com>
2023-10-12 20:45:47 +0900

    upload

--- video_streaming/client.py
+++ video_streaming/client.py
@@ -6,18 +6,19 @@ import numpy as np
 from PIL import Image, ImageTk
 
 # 서버 IP 주소 및 포트 번호
-SERVER_IP = '127.0.0.1'
-SERVER_PORT = 12345
+SERVER_IP = '93.184.216.34'
+SERVER_PORT = 3001
 
 # 웹캠 캡처를 위한 스레드
 class VideoStreamThread(threading.Thread):
     def __init__(self, server_socket):
         super().__init__()
         self.server_socket = server_socket
+        self.is_running = True
 
     def run(self):
         cap = cv2.VideoCapture(0)  # 웹캠 캡처
-        while True:
+        while self.is_running:
             ret, frame = cap.read()
             if not ret:
                 break
@@ -39,8 +40,8 @@ frame.pack()
 
 # 서버로부터 비디오 스트리밍을 받아 화면에 표시하는 함수
 def receive_video_stream():
-    while True:
-        try:
+    try:
+        while True:
             img_bytes = client_socket.recv(1024)
             img_encoded = np.frombuffer(img_bytes, dtype=np.uint8)
             frame = cv2.imdecode(img_encoded, cv2.IMREAD_COLOR)
@@ -48,20 +49,21 @@ def receive_video_stream():
             photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
             frame_label.config(image=photo)
             frame_label.image = photo
-        except Exception as e:
-            print(e)
-            break
+    except (ConnectionResetError, ConnectionAbortedError):
+        print("서버와의 연결이 끊어졌습니다.")
+    except Exception as e:
+        print("에러 발생:", e)
 
 # 비디오 수신 스레드 시작
-video_thread = threading.Thread(target=receive_video_stream)
-video_thread.daemon = True
+video_thread = VideoStreamThread(client_socket)
 video_thread.start()
 
 # 메시지 전송 함수
 def send_message():
     message = entry.get()
-    client_socket.sendall(message.encode())
-    entry.delete(0, tk.END)
+    if message:
+        client_socket.sendall(message.encode())
+        entry.delete(0, tk.END)
 
 # GUI 구성요소 생성
 entry = tk.Entry(root, width=50)
@@ -69,8 +71,16 @@ entry.pack()
 send_button = tk.Button(root, text="Send", command=send_message)
 send_button.pack()
 
+# GUI 종료 시 클라이언트 정리
+def close_client():
+    video_thread.is_running = False
+    video_thread.join()
+    client_socket.close()
+    root.destroy()
+
+# GUI 종료 버튼
+close_button = tk.Button(root, text="Close", command=close_client)
+close_button.pack()
+
 # GUI 시작
 root.mainloop()
-
-# 연결 종료 시 스레드 및 소켓 닫기
-client_socket.close()
--- video_streaming/server.py
+++ video_streaming/server.py
@@ -1,16 +1,29 @@
 import tkinter as tk
 import cv2
 from PIL import Image, ImageTk
+import threading
 
-# 화면 갱신 함수
-def update():
-    ret, frame = cap.read()
-    if ret:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
-        label.config(image=photo)
-        label.image = photo
-    window.after(10, update)
+# 웹캠 캡처를 위한 스레드
+class VideoCaptureThread(threading.Thread):
+    def __init__(self, window):
+        super().__init__()
+        self.window = window
+        self.cap = cv2.VideoCapture(0)
+        self.stop_event = threading.Event()
+
+    def run(self):
+        while not self.stop_event.is_set():
+            ret, frame = self.cap.read()
+            if ret:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+                label.config(image=photo)
+                label.image = photo
+                label.update()
+        self.cap.release()
+
+    def stop(self):
+        self.stop_event.set()
 
 # 메시지 보내기 함수
 def send_message():
@@ -20,12 +33,18 @@ def send_message():
     chat_text.config(state=tk.DISABLED)
     entry.delete(0, tk.END)
 
+# 종료 함수
+def on_closing():
+    video_thread.stop()
+    window.destroy()
+
 # GUI 초기화
 window = tk.Tk()
 window.title("화상 채팅")
 
 # 웹캠 초기화
-cap = cv2.VideoCapture(0)
+video_thread = VideoCaptureThread(window)
+video_thread.start()
 
 # 라벨 위젯을 사용하여 영상 표시 (80%)
 label = tk.Label(window)
@@ -43,16 +62,13 @@ entry.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
 send_button = tk.Button(window, text="보내기", command=send_message)
 send_button.grid(row=1, column=1, padx=10, pady=10, sticky="se")
 
+# 종료 시 이벤트 처리
+window.protocol("WM_DELETE_WINDOW", on_closing)
+
 # 행 및 열 가중치 설정
 window.grid_rowconfigure(0, weight=1)
 window.grid_columnconfigure(0, weight=4)  # 비디오 화면이 80% 차지
 window.grid_columnconfigure(1, weight=1)  # 채팅 창이 20% 차지
 
-# 갱신 함수 호출
-update()
-
 # GUI 시작
 window.mainloop()
-
-# 웹캠 해제
-cap.release()

f674fd640b69d8a3aec4d097b70286363048a02b
Shin suho <tngh0203@gmail.com>
2023-10-12 18:32:26 +0900

    upload

--- video_streaming/client.py
+++ video_streaming/client.py
@@ -1,72 +1,76 @@
-import socket
+import tkinter as tk
 import threading
 import cv2
+import socket
 import numpy as np
+from PIL import Image, ImageTk
 
-# 서버 설정
-server_ip = '127.0.0.1'
-server_port = 8888
+# 서버 IP 주소 및 포트 번호
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 12345
 
-# 소켓 생성
+# 웹캠 캡처를 위한 스레드
+class VideoStreamThread(threading.Thread):
+    def __init__(self, server_socket):
+        super().__init__()
+        self.server_socket = server_socket
+
+    def run(self):
+        cap = cv2.VideoCapture(0)  # 웹캠 캡처
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            _, img_encoded = cv2.imencode('.jpg', frame)
+            img_bytes = img_encoded.tobytes()
+            self.server_socket.sendall(img_bytes)
+
+# 클라이언트 소켓 설정
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((server_ip, server_port))
+client_socket.connect((SERVER_IP, SERVER_PORT))
 
+# GUI 생성
+root = tk.Tk()
+root.title("Video Streaming Client")
 
-# 서버로부터 비디오 정보를 받는 함수
-def receive_video():
+# 비디오 프레임 표시
+frame = tk.Label(root)
+frame.pack()
+
+# 서버로부터 비디오 스트리밍을 받아 화면에 표시하는 함수
+def receive_video_stream():
     while True:
         try:
-            # 프레임 크기 수신
-            frame_size = int(client_socket.recv(10).decode('utf-8'))
-
-            # 프레임 데이터 수신
-            frame_data = b''
-            remaining_size = frame_size
-            while remaining_size > 0:
-                chunk = client_socket.recv(remaining_size)
-                if not chunk:
-                    break
-                frame_data += chunk
-                remaining_size -= len(chunk)
-
-            # 수신한 데이터를 이미지로 디코딩
-            frame_array = np.frombuffer(frame_data, dtype=np.uint8)
-            frame = cv2.imdecode(frame_array, cv2.IMREAD_COLOR)
-
-            # 화면에 비디오 프레임 표시
-            cv2.imshow('Video', frame)
-            cv2.waitKey(1)  # 1ms 대기 후 다음 프레임 표시
-        except:
-            # 예외 발생 시 연결 종료
-            client_socket.close()
+            img_bytes = client_socket.recv(1024)
+            img_encoded = np.frombuffer(img_bytes, dtype=np.uint8)
+            frame = cv2.imdecode(img_encoded, cv2.IMREAD_COLOR)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+            frame_label.config(image=photo)
+            frame_label.image = photo
+        except Exception as e:
+            print(e)
             break
 
+# 비디오 수신 스레드 시작
+video_thread = threading.Thread(target=receive_video_stream)
+video_thread.daemon = True
+video_thread.start()
 
 # 메시지 전송 함수
 def send_message():
-    while True:
-        message = input()
-        client_socket.send(message.encode('utf-8'))
+    message = entry.get()
+    client_socket.sendall(message.encode())
+    entry.delete(0, tk.END)
 
+# GUI 구성요소 생성
+entry = tk.Entry(root, width=50)
+entry.pack()
+send_button = tk.Button(root, text="Send", command=send_message)
+send_button.pack()
 
-# 채팅 수신 함수
-def receive_message():
-    while True:
-        try:
-            message = client_socket.recv(1024).decode('utf-8')
-            print(message)
-        except:
-            # 예외 발생 시 연결 종료
-            client_socket.close()
-            break
+# GUI 시작
+root.mainloop()
 
-
-# 비디오 수신 스레드 시작
-video_receive_thread = threading.Thread(target=receive_video)
-video_receive_thread.start()
-
-# 클라이언트 채팅 수신 및 전송 스레드 시작
-receive_thread = threading.Thread(target=receive_message)
-send_thread = threading.Thread(target=send_message)
-receive_thread.start()
-send_thread.start()
+# 연결 종료 시 스레드 및 소켓 닫기
+client_socket.close()
--- video_streaming/server.py
+++ video_streaming/server.py
@@ -1,88 +1,58 @@
-import socket
-import threading
+import tkinter as tk
 import cv2
-import numpy as np
+from PIL import Image, ImageTk
 
-# 서버 설정
-server_ip = '127.0.0.1'
-server_port = 8888
+# 화면 갱신 함수
+def update():
+    ret, frame = cap.read()
+    if ret:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+        label.config(image=photo)
+        label.image = photo
+    window.after(10, update)
 
-# 소켓 생성
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((server_ip, server_port))
-server_socket.listen(5)
-print(f"서버가 {server_ip}:{server_port}에서 실행 중입니다.")
+# 메시지 보내기 함수
+def send_message():
+    message = entry.get()
+    chat_text.config(state=tk.NORMAL)
+    chat_text.insert(tk.END, "나: " + message + "\n")
+    chat_text.config(state=tk.DISABLED)
+    entry.delete(0, tk.END)
 
-# 연결된 클라이언트를 저장할 리스트
-clients = []
+# GUI 초기화
+window = tk.Tk()
+window.title("화상 채팅")
 
-# 비디오 캡처 설정
-cap = cv2.VideoCapture(r'C:\Users\user\Videos\Eternal Return  Black Survival\sample_video.mp4')
+# 웹캠 초기화
+cap = cv2.VideoCapture(0)
 
-# 비디오 파일명이나 카메라 디바이스를 지정하세요
+# 라벨 위젯을 사용하여 영상 표시 (80%)
+label = tk.Label(window)
+label.grid(row=0, column=0, padx=10, pady=10, rowspan=2, sticky="nsew")
 
-# 클라이언트에게 비디오 정보를 전송하는 함수
-def send_video_info(client_socket):
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        # 비디오 프레임을 클라이언트에게 전송
-        _, frame_encoded = cv2.imencode('.jpg', frame)
-        frame_bytes = frame_encoded.tobytes()
-        frame_size = len(frame_bytes)
+# 채팅 창 (Text 위젯) 추가 (20%)
+chat_text = tk.Text(window, wrap=tk.WORD, state=tk.DISABLED)
+chat_text.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
 
-        # 프레임 크기를 먼저 전송
-        client_socket.send(str(frame_size).encode('utf-8'))
+# 메시지 입력 필드 (20%)
+entry = tk.Entry(window)
+entry.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
 
-        # 프레임 데이터를 전송
-        client_socket.send(frame_bytes)
+# 메시지 보내기 버튼 (20%)
+send_button = tk.Button(window, text="보내기", command=send_message)
+send_button.grid(row=1, column=1, padx=10, pady=10, sticky="se")
 
+# 행 및 열 가중치 설정
+window.grid_rowconfigure(0, weight=1)
+window.grid_columnconfigure(0, weight=4)  # 비디오 화면이 80% 차지
+window.grid_columnconfigure(1, weight=1)  # 채팅 창이 20% 차지
 
-# 클라이언트와의 채팅을 처리하는 스레드 함수
-def handle_client(client_socket):
-    while True:
-        try:
-            message = client_socket.recv(1024).decode('utf-8')
-            if not message:
-                # 클라이언트가 연결을 끊었을 경우
-                remove_client(client_socket)
-            else:
-                # 채팅 메시지를 모든 클라이언트에게 전송
-                print(f"받은 메시지: {message}")
-                broadcast(message)
-        except:
-            # 예외 발생 시 클라이언트 연결 종료
-            remove_client(client_socket)
+# 갱신 함수 호출
+update()
 
+# GUI 시작
+window.mainloop()
 
-# 클라이언트를 리스트에 추가
-def add_client(client_socket):
-    clients.append(client_socket)
-
-
-# 클라이언트를 리스트에서 제거
-def remove_client(client_socket):
-    if client_socket in clients:
-        clients.remove(client_socket)
-
-
-# 모든 클라이언트에게 메시지 전송
-def broadcast(message):
-    for client in clients:
-        client.send(message.encode('utf-8'))
-
-
-# 클라이언트 연결 대기 및 처리
-while True:
-    client_socket, client_address = server_socket.accept()
-    print(f"새로운 연결 수락: {client_address}")
-    add_client(client_socket)
-
-    # 클라이언트에게 비디오 정보를 전송하는 스레드 시작
-    video_info_thread = threading.Thread(target=send_video_info, args=(client_socket,))
-    video_info_thread.start()
-
-    # 클라이언트 채팅 처리 스레드 시작
-    client_handler = threading.Thread(target=handle_client, args=(client_socket,))
-    client_handler.start()
+# 웹캠 해제
+cap.release()
